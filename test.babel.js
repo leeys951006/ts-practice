06447ed46c57dbb76fe80252f8cdeadda45965f5
이연승 <leeys951006@gmail.com>
2024-07-05 17:53:40 +0900

    npx babel test.ts -o test.babel.js 실행

---
+++ test.babel.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function add(a, b) {
+  var result = a + b;
+  return result;
+}
+function minam() {
+  var obj = {
+    name: '이연승',
+    age: 30,
+    job: '무직'
+  };
+  return obj;
+}
