e07169e0046071aa0e46fdb9b7a473ccff87a95f
이연승 <leeys951006@gmail.com>
2024-07-05 17:40:17 +0900

    app.js 내용 수정

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-function a(a, b) {
-  let result = a + b;
-  return result;
+function a(...args) {
+  let result = args[0] + args[1];
+  const oneMore = () => {
+    return result + 10;
+  };
+  return oneMore();
 }
